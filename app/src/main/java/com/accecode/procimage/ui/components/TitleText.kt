c41a0763820c56675a461d2b30ea0f67e8a99154
alexiscs.dev <alexiscs.dev@gmail.com>
2023-09-10 15:25:17 -0500

    modify titleText adding style

--- app/src/main/java/com/accecode/procimage/ui/components/TitleText.kt
+++ app/src/main/java/com/accecode/procimage/ui/components/TitleText.kt
@@ -2,8 +2,11 @@ package com.accecode.procimage.ui.components
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TitleText(text: String) {
-    Text(text = text)
+    //custom text
+    Text( text = text.uppercase(), fontSize = 16.sp, fontWeight = FontWeight.Bold)
 }
